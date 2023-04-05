3ab77276ee5d5e0e58caca0d66581cc00766e158
Alexandra Skripkina <aa.skripkina@gmail.com>
2023-04-05 11:56:41 +0300

    modified

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -10,7 +10,7 @@ import java.time.format.DateTimeFormatter;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
-    public String generateDate(long addDays, String pattern) {
+    public String setUpDate(long addDays, String pattern) {
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
 
@@ -18,19 +18,19 @@ public class CardDeliveryTest {
     @Test
     void shouldSubmitRequest(){
         Configuration.holdBrowserOpen = true;
-        String planningDate = generateDate(3, "dd.MM.yyyy");
+        String newDate = setUpDate(3, "dd.MM.yyyy");
 
         open("http://localhost:9999/");
         $x("//*[@data-test-id = 'city']//input").setValue("Петрозаводск");
         $x("//*[@data-test-id = 'date']//input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $x("//*[@data-test-id = 'date']//input").setValue(planningDate);
+        $x("//*[@data-test-id = 'date']//input").setValue(newDate);
         $x("//*[@data-test-id = 'name']//input").setValue("Иван Иванов");
         $x("//*[@data-test-id = 'phone']//input").setValue("+78123456789");
         $x("//*[@data-test-id = 'agreement']").click();
         $x("//*[text() = 'Забронировать']").click();
 
         $(".notification__content")
-                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + newDate), Duration.ofSeconds(15))
                 .shouldBe(Condition.visible);
     }
 }
